b9c905a0453e318d947e862c1fefe4cdf6ced538
eeeseohyun <110715190+eeeseohyun@users.noreply.github.com>
2023-01-20 20:25:08 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    printf("         ,r'\"7");
+    printf("\nr`-_   ,'  ,/");
+    printf("\n \\. \". L_r'");
+    printf("\n   `~\\/");
+    printf("\n      |");
+    printf("\n      |\n");
+	return 0;
+}
