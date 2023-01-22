94334242bc5ac6a790232afd281d32e99e80e5f5
eeeseohyun <eeeseohyun@gmail.com>
2023-01-22 17:46:16 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,11 @@
+#include<iostream>
+int main(){
+    using namespace std;
+    int n,a,b;
+    cin >> n;
+    for(int i=0; i<n; i++){
+        int a,b;
+        cin >> a >> b;
+        cout << a+b << endl;
+    }
+}
