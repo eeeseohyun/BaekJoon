165b28e6c98ab988f1e329b8b079558cf7f178b6
eeeseohyun <eeeseohyun@gmail.com>
2023-01-22 18:28:31 +0900

    [Bronze V] Title: A+B - 5, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,14 @@
+#include<iostream>
+int main(){
+    using namespace std;
+    int a,b;
+
+    while(a!=0||b!=0){
+        cin>>a>>b;
+        if(a!=0||b!=0){
+            cout << a+b << endl;
+        }else{
+            break;
+        }
+}
+}
