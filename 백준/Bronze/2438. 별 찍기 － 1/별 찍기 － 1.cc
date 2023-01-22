2e692dfede431571604e5c6066dae611d7a06d3a
eeeseohyun <eeeseohyun@gmail.com>
2023-01-22 18:18:22 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,13 @@
+#include<iostream>
+int main(){
+    using namespace std;
+    int n;
+    cin >> n;
+    for(int k=1; k<=n; k++){
+       for(int i=1; i<=k; i++){
+        cout << "*";
+    } 
+        cout << endl;
+  }
+    
+}
