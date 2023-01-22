00ce8dc08ff0483da3de01b84b4357d3a4e82a2e
eeeseohyun <eeeseohyun@gmail.com>
2023-01-22 17:42:22 +0900

    [Bronze V] Title: 구구단, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.cc
@@ -0,0 +1,9 @@
+#include<iostream>
+int main(){
+    using namespace std;
+    int n;
+    cin >> n;
+    for(int i=1; i<10; i++){
+     cout << n << " * " << i << " = " << n*i << endl;   
+    } 
+}
