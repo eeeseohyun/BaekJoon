20753d41534797ba3ef1e8ce6089365e695562d3
eeeseohyun <eeeseohyun@gmail.com>
2023-01-22 18:08:51 +0900

    [Bronze V] Title: A+B - 7, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,12 @@
+#include<iostream>
+int main(){
+    using namespace std;
+    int n;
+    cin >> n;
+    for(int i=0; i<n; i++){
+        int a,b;
+        cin >> a >> b;
+        cout<<"Case #"<<i+1<<": "<<a+b<<endl;
+    }
+}
+    
